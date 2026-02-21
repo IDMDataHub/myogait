05a94e255fa82c61555988109cc6897254bef34d
Frédéric Fer <f.fer@institut-myologie.org>
2026-02-21 20:39:01 +0100

    chore(release): bump version to 0.3.4

--- myogait/__init__.py
+++ myogait/__init__.py
@@ -40,7 +40,7 @@ Validation::
     report = validate_biomechanical(data, cycles)
 """
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 from .extract import extract, detect_sagittal_alignment, auto_crop_roi, select_person
 from .normalize import (
